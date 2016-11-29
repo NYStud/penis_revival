9f744894d00591ad4f8b3f2ec04ed4c11531903d
jemus42 <jemus42@quantenbrot.de>
2016-11-29 20:32:23 +0100

    Enable code display, narrower code formatting

--- setup.R
+++ setup.R
@@ -12,6 +12,3 @@ library(tadaatoolbox)
 # devtools::install_github("tadaadata/loldata")
 penis <- loldata::penis
 
-## knitr options
-knitr::opts_chunk$set(echo = F, warning = F, message = F, fig.align = "center",
-                      fig.path = "assets/plots/")
