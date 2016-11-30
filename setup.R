004a5d9777e38634eb955d1e7a560baa3bac3bfb
jemus42 <jemus42@quantenbrot.de>
2016-11-30 13:44:27 +0100

    Add long format dataset

--- setup.R
+++ setup.R
@@ -12,3 +12,13 @@ library(tadaatoolbox)
 # devtools::install_github("tadaadata/loldata")
 penis <- loldata::penis
 
+penis_long <-
+  penis %>%
+  select(-circumf_erect_in, -length_erect_in) %>%
+  gather(state_l, length,  length_flaccid, length_erect) %>%
+  gather(state_v, volume,  volume_flaccid, volume_erect) %>%
+  gather(state_c, circumf, circumf_flaccid, circumf_erect) %>%
+  gather(state_rel, state, starts_with("state_")) %>%
+  mutate(state = ifelse(grepl(x = state, pattern = "erect"), "Erect", "Flaccid"),
+         state = factor(state, levels = c("Flaccid", "Erect"), ordered = T)) %>%
+  select(-state_rel)
